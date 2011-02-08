56e616fb7f292da505b1f2977414c0fe2186e7ce
unknown <Le Raton Laveur@.(none)>
2011-02-08 16:39:07 +0100

    Ajout du type "textarea" pour les options

---
+++ helpers/options/textarea.php
@@ -0,0 +1,8 @@
+<tr>
+    <th scope="row">
+        <label for="<?php echo $id; ?>"><?php echo $name; ?></label>
+    </th>
+    <td>
+        <textarea type="text" id="<?php echo $id; ?>" name="<?php echo $id; ?>" style="width:98%; height:50px; "><?php echo $value; ?></textarea>
+    </td>
+</tr>
