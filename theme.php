6d91e90465defdc32a02d3b40003f019d2245dbc
Grafikart <contact@grafikart.fr>
2012-10-08 20:13:50 +0200

    Fixed layout of upload meta type

--- theme.php
+++ theme.php
@@ -164,7 +164,12 @@ class Theme {
      * */
     function images(){
         foreach($this->options['images'] as $post_type=>$formats){
-            if ((isset($_POST['action']) && $_POST['action'] == 'ajax_thumbnail_rebuild') || (isset($_GET['page']) && $_GET['page'] == 'ajax-thumbnail-rebuild') || (isset($_REQUEST['post_id']) && get_post_type($_REQUEST['post_id']) == $post_type) || (isset($_REQUEST['action']) && $_REQUEST['action'] == 'delete')) {
+            if (
+                (isset($_POST['action']) && $_POST['action'] == 'ajax_thumbnail_rebuild') ||
+                (isset($_GET['page']) && $_GET['page'] == 'ajax-thumbnail-rebuild') ||
+                (isset($_REQUEST['post_id']) && get_post_type($_REQUEST['post_id']) == $post_type) ||
+                (isset($_REQUEST['action']) && $_REQUEST['action'] == 'delete')
+            ) {
                 foreach($formats as $f){
                     if($f[0]=='thumb'){
                         set_post_thumbnail_size( $f[1], $f[2], $f[3] );
