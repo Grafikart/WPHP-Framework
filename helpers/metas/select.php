343394ea783ca7afe57c1b87666fdc1023ae884a
Luc D <taliesinpenbardd@gmail.com>
2013-09-02 20:58:24 +0200

    pfff... correction de coquille

--- helpers/metas/select.php
+++ helpers/metas/select.php
@@ -7,5 +7,5 @@
 			<option value="<?php echo $id; ?>" <?php if($id==$value){ echo 'selected'; } ?>><?php echo $v; ?></option>
 		<?php endforeach; ?>
 	</select><br />
-	<?php if( isset ( $comment )) : ?><p><?php echo $commnet; ?></p><?php endif; ?>
+	<?php if( isset ( $comment )) : ?><p><?php echo $comment; ?></p><?php endif; ?>
 </div>
